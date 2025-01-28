46fa192f6bc67025d2610e200a8cb47389731616
Hassan Rasoulpour <hassan.rasoulpour1@gmail.com>
2025-01-29 00:24:03 +0330

    Improme search and insert algorithms

--- search_insert.py
+++ search_insert.py
@@ -1,26 +1,15 @@
 def search_insert(array, value):
     start_index = 0
     end_index = len(array) - 1
-    mid = end_index // 2
-    new_array = []
-    
+
     while start_index <= end_index:
+        mid = (start_index + end_index) // 2
         if value < array[mid]:
-            mid -= 1
-            end_index = mid
+            end_index = mid - 1
         else:
-            mid += 1
-            start_index = mid
+            start_index = mid + 1
 
-    if start_index == 0:
-            new_array.append(value)
-            new_array.extend(array)
-    elif 0 < start_index < len(array):
-        new_array.extend(array[:start_index])
-        new_array.append(value)
-        new_array.extend(array[start_index:])
-    else:
-        new_array.extend(array)
-        new_array.append(value)
+    # insert value to array
+    array[start_index:start_index] = [value]
 
-    return new_array
+    return array
